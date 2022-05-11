7ca08b1f112160bb76f6299bc0116bee50d3a81c
iamAndreyGavrilov <79842149+iamAndreyGavrilov@users.noreply.github.com>
2022-05-11 21:58:30 +0500

    Update 4.13 Приведение типов.ts

--- продвинутые типы/4.13 Приведение типов.ts
+++ продвинутые типы/4.13 Приведение типов.ts
@@ -5,3 +5,32 @@ const f: boolean = new Boolean(a).valueOf();
 
 let c = "5";
 let d: number = parseInt(c);
+
+interface User {
+  name: string;
+  email: string;
+  login: string;
+}
+
+const user: User = {
+  name: "Vasya",
+  email: "a@a.ru",
+  login: "qwerty123",
+};
+
+interface Admin {
+  name: string;
+  role: number;
+}
+
+const admin: Admin = {
+  ...user,
+  role: 1,
+};
+
+function userToAdmin(user: User): Admin { 
+  return {
+    name: user.name,
+    role: 1,
+  };
+}
