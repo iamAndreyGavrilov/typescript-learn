8d812425cc1189741617564cd1256378b434c6cc
Andrey Gavrilov <boombardir20@gmail.com>
2022-05-12 16:27:22 +0500

    w2

--- продвинутые типы/HomeWork/work1.ts
+++ продвинутые типы/HomeWork/work1.ts
@@ -1,43 +1,43 @@
-//Упражнение - Типизируем ответ сервера
+// //Упражнение - Типизируем ответ сервера
 
-interface IPayment {
-  sum: number;
-  from: number;
-  to: number;
-}
-
-interface IPaymentRequest extends IPayment {}
-
-enum PaymentStatus {
-  Success = "success",
-  Failed = "failed",
-}
-
-interface IDataSuccess extends IPayment {
-  databaseId: number;
-}
-
-interface IDataFailed {
-  errorMessage: string;
-  errorCode: number;
-}
-
-// 1
-interface IResponse {
-  status: PaymentStatus;
-  data: IDataSuccess | IDataFailed;
-}
-
-// 2
-interface IResponseSuccess {
-  status: PaymentStatus.Success;
-  data: IDataSuccess;
-}
-interface IResponseFailed {
-  status: PaymentStatus.Failed;
-  data: IDataFailed;
-}
-
-// function get(): IResponseSuccess | IResponseFailed {
-//   return;
+// interface IPayment {
+//   sum: number;
+//   from: number;
+//   to: number;
 // }
+
+// interface IPaymentRequest extends IPayment {}
+
+// enum PaymentStatus {
+//   Success = "success",
+//   Failed = "failed",
+// }
+
+// interface IDataSuccess extends IPayment {
+//   databaseId: number;
+// }
+
+// interface IDataFailed {
+//   errorMessage: string;
+//   errorCode: number;
+// }
+
+// // 1
+// interface IResponse {
+//   status: PaymentStatus;
+//   data: IDataSuccess | IDataFailed;
+// }
+
+// // 2
+// interface IResponseSuccess {
+//   status: PaymentStatus.Success;
+//   data: IDataSuccess;
+// }
+// interface IResponseFailed {
+//   status: PaymentStatus.Failed;
+//   data: IDataFailed;
+// }
+
+// // function get(): IResponseSuccess | IResponseFailed {
+// //   return;
+// // }
