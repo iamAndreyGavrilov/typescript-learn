a0afa6f2091465da85512a852fc98fc0b5db1c06
Andrey Gavrilov <boombardir20@gmail.com>
2022-05-12 14:33:15 +0500

    1

--- продвинутые типы/4.13 Приведение типов.ts
+++ продвинутые типы/4.13 Приведение типов.ts
@@ -1,36 +1,36 @@
-const a = 5;
-const b: string = a.toString();
-const e: string = new String(a).valueOf();
-const f: boolean = new Boolean(a).valueOf();
+// const a = 5;
+// const b: string = a.toString();
+// const e: string = new String(a).valueOf();
+// const f: boolean = new Boolean(a).valueOf();
 
-let c = "5";
-let d: number = parseInt(c);
+// let c = "5";
+// let d: number = parseInt(c);
 
-interface User {
-  name: string;
-  email: string;
-  login: string;
-}
+// interface User {
+//   name: string;
+//   email: string;
+//   login: string;
+// }
 
-const user: User = {
-  name: "Vasya",
-  email: "a@a.ru",
-  login: "qwerty123",
-};
+// const user: User = {
+//   name: "Vasya",
+//   email: "a@a.ru",
+//   login: "qwerty123",
+// };
 
-interface Admin {
-  name: string;
-  role: number;
-}
+// interface Admin {
+//   name: string;
+//   role: number;
+// }
 
-const admin: Admin = {
-  ...user,
-  role: 1,
-};
+// const admin: Admin = {
+//   ...user,
+//   role: 1,
+// };
 
-function userToAdmin(user: User): Admin { 
-  return {
-    name: user.name,
-    role: 1,
-  };
-}
+// function userToAdmin(user: User): Admin { 
+//   return {
+//     name: user.name,
+//     role: 1,
+//   };
+// }
