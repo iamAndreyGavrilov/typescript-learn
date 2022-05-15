bbc0152679fffe0e4ac806003549aecbc016b50d
iamAndreyGavrilov <79842149+iamAndreyGavrilov@users.noreply.github.com>
2022-05-15 17:14:37 +0500

    1

--- 3 классы/5.4 Методы.ts
+++ 3 классы/5.4 Методы.ts
@@ -1,36 +1,36 @@
-// enum PaymentStatus {
-//   Holded,
-//   Processed,
-//   Reversed,
-// }
+enum PaymentStatus {
+  Holded,
+  Processed,
+  Reversed,
+}
 
-// class Payment {
-//   id: number;
-//   status: PaymentStatus = PaymentStatus.Holded;
-//   createdAt: Date = new Date();
-//   updateAt: Date;
+class Payment {
+  id: number;
+  status: PaymentStatus = PaymentStatus.Holded;
+  createdAt: Date = new Date();
+  updateAt: Date;
 
-//   constructor(id: number) {
-//     this.id = id;
-//   }
+  constructor(id: number) {
+    this.id = id;
+  }
 
-//   getPaymentLifeTime(): number {
-//     return new Date().getTime() - this.createdAt.getTime();
-//   }
+  getPaymentLifeTime(): number {
+    return new Date().getTime() - this.createdAt.getTime();
+  }
 
-//   unholdPayment(): void {
-//     if (this.status === PaymentStatus.Processed) {
-//       throw new Error("Платеж в процессе оплаты вернуть нельзя");
-//     }
+  unholdPayment(): void {
+    if (this.status === PaymentStatus.Processed) {
+      throw new Error("Платеж в процессе оплаты вернуть нельзя");
+    }
 
-//     this.status = PaymentStatus.Reversed;
-//     this.updateAt = new Date();
-//   }
-// }
+    this.status = PaymentStatus.Reversed;
+    this.updateAt = new Date();
+  }
+}
 
-// const payment = new Payment(1);
-// payment.unholdPayment();
-// console.log(payment);
+const payment = new Payment(1);
+payment.unholdPayment();
+console.log(payment);
 
-// const time = payment.getPaymentLifeTime();
-// console.log(time);
+const time = payment.getPaymentLifeTime();
+console.log(time);
