b8bb4878eeac362e2ec7ac80fdd0378528588379
iamAndreyGavrilov <79842149+iamAndreyGavrilov@users.noreply.github.com>
2022-05-15 16:29:01 +0500

    123

--- 3 классы/5.2 CreateСlass.js
+++ 3 классы/5.2 CreateСlass.js
@@ -1,10 +1,17 @@
 "use strict";
-class User {
-    constructor(name) {
-        this.name = name;
-    }
-}
-const user = new User("Vasya");
-console.log(user);
-user.name = "Petya";
-console.log(user);
+// class User {
+//   name: string;
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
+// const user = new User("Vasya");
+// console.log(user);
+// user.name = "Petya";
+// console.log(user);
+// //tsconfig - strictPropertyInitialization: false
+// class Admin {
+//   role: number;
+// }
+// const admin = new Admin();
+// admin.role = 1;
--- 3 классы/5.2 CreateСlass.ts
+++ 3 классы/5.2 CreateСlass.ts
@@ -1,12 +1,21 @@
-class User {
-  name: string;
+// class User {
+//   name: string;
 
-  constructor(name: string) {
-    this.name = name;
-  }
-}
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
 
-const user = new User("Vasya");
-console.log(user);
-user.name = "Petya";
-console.log(user);
+// const user = new User("Vasya");
+// console.log(user);
+// user.name = "Petya";
+// console.log(user);
+
+// //tsconfig - strictPropertyInitialization: false
+
+// class Admin {
+//   role: number;
+// }
+
+// const admin = new Admin();
+// admin.role = 1;
