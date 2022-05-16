49a14b56639da8a02794d1bd3921860cafa2a94d
iamAndreyGavrilov <79842149+iamAndreyGavrilov@users.noreply.github.com>
2022-05-16 15:48:49 +0500

    1

---
+++ 3 классы/5.7 Implements.ts
@@ -0,0 +1,32 @@
+// interface ILogger {
+//   log(...args): void;
+//   error(...args): void;
+// }
+
+// class Logger implements ILogger {
+//   log(...args: any[]): void {
+//     console.log(...args);
+//   }
+//   error(...args: any[]): void {
+//     // Кинуть во внешнюю систему
+//     console.log(...args);
+//   }
+// }
+
+// interface IPayable {
+//   pay(paymentId: number): void;
+//   price?: number;
+// }
+
+// interface IDeletable {
+//   delete(): void;
+// }
+
+// class User implements IPayable, IDeletable {
+//   delete(): void {
+// 	  throw new Error("Method not implemented.");
+//   }
+//   pay(paymentId: number | string): void {
+//     ///
+//   }
+// }
