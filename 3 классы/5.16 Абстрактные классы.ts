26992ff4fa3e54e126493a89b03ba94704e066c5
Andrey Gavrilov <boombardir20@gmail.com>
2022-05-20 12:17:57 +0500

    1

--- 3 классы/5.16 Абстрактные классы.ts
+++ 3 классы/5.16 Абстрактные классы.ts
@@ -1,20 +1,20 @@
-abstract class Controller {
-  abstract handle(req: any): void;
+// abstract class Controller {
+//   abstract handle(req: any): void;
 
-  // такого мы не может сделать в interfaces, т.к он описывает только форму class
-  handleWithLogs(req: any) {
-    console.log("Start");
-    this.handle(req);
-    console.log("End");
-  }
-}
+//   // такого мы не может сделать в interfaces, т.к он описывает только форму class
+//   handleWithLogs(req: any) {
+//     console.log("Start");
+//     this.handle(req);
+//     console.log("End");
+//   }
+// }
 
-class UserController extends Controller {
-  handle(req: any): void {
-    console.log(req);
-  }
-}
+// class UserController extends Controller {
+//   handle(req: any): void {
+//     console.log(req);
+//   }
+// }
 
-// new Controller() - Невозможно создать экземпляр абстрактного класса
-const c = new UserController();
-c.handleWithLogs("Request");
+// // new Controller() - Невозможно создать экземпляр абстрактного класса
+// const c = new UserController();
+// c.handleWithLogs("Request");
