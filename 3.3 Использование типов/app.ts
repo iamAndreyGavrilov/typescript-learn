7f809322a91ae10bdc02546a886d5d8e53b03be9
Andrey G <a.gavrilov@promo-bot.ru>
2022-08-01 11:46:00 +0500

    1

---
+++ 3.3 Использование типов/app.ts
@@ -0,0 +1,9 @@
+// const a = 1;
+
+// let income: number = 1000;
+// let bonus: number = 500;
+// let res: number = income + bonus;
+// let b: string = 'abc';
+// let c: boolean = true;
+
+// console.log(res);
