d63b3cb621c51f7df41988ad1c2afcc7d55a9f57
Andrey G <a.gavrilov@promo-bot.ru>
2022-08-05 18:03:36 +0500

    1

---
+++ 5.3 Конструктор/app.ts
@@ -0,0 +1,26 @@
+// class User {
+//   name: string;
+//   age: number;
+
+//   //overLoad
+//   constructor();
+//   constructor(name: string);
+//   constructor(age: number);
+//   constructor(name: string, age: number);
+
+//   //constructor имплементации
+//   constructor(ageOrName?: string | number, age?: number) {
+//     if (typeof ageOrName === 'string') {
+//       this.name = ageOrName;
+//     } else if (typeof ageOrName === 'number') {
+//       this.age = ageOrName;
+//     } else if (typeof age === 'number') {
+//       this.age = age;
+//     }
+//   }
+// }
+
+// const user = new User('Vasia');
+// const user2 = new User();
+// const user3 = new User(33);
+// const user4 = new User('Vasia', 33);
