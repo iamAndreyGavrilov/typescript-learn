ea0c63c40d2a3400acaca261f9983ec4dc663d42
Andrey G <a.gavrilov@promo-bot.ru>
2022-08-06 21:09:55 +0500

    1

---
+++ 5.2 Создание класса/app.js
@@ -0,0 +1,17 @@
+"use strict";
+// class User {
+//   name: string;
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
+// const user = new User('Vasia');
+// console.log(user);
+// user.name = 'Petya';
+// console.log(user);
+// //"strictPropertyInitialization": false,
+// class Admin {
+//   role: number;
+// }
+// const admin = new Admin();
+// admin.role = 1;
