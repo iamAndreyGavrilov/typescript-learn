29a7f768ff7ad49ddffca0f23ecf290c0bd77b7a
Andrey Gavrilov <a.gavrilov@promo-bot.ru>
2022-08-09 17:55:08 +0500

    5.10

--- 5.10 Композиция против наследования/app.ts
+++ 5.10 Композиция против наследования/app.ts
@@ -1,34 +1,56 @@
-class User {
-  name: string;
+// class User {
+//   name: string;
 
-  constructor(name: string) {
-    this.name = name;
-  }
-}
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
 
-//Наследование
-class Users extends Array<User> {
-  searchByName(name: string) {
-    return this.filter((u) => u.name === name);
-  }
+// //Наследование
+// class Users extends Array<User> {
+//   searchByName(name: string) {
+//     return this.filter((u) => u.name === name);
+//   }
 
-  override toString(): string {
-    return this.map((u) => u.name).join(', ');
-  }
-}
+//   override toString(): string {
+//     return this.map((u) => u.name).join(', ');
+//   }
+// }
 
-const users = new Users();
+// const users = new Users();
 
-users.push(new User('Vasia'));
-users.push(new User('Petya'));
+// users.push(new User('Vasia'));
+// users.push(new User('Petya'));
 
-console.log(users.toString());
+// console.log(users.toString());
 
-//композиция - она приоритетнее
-class UserList {
-  users: User[];
+// //композиция - она приоритетнее
+// class UserList {
+//   users: User[];
 
-  push(u: User) {
-    this.users.push(u);
-  }
-}
+//   push(u: User) {
+//     this.users.push(u);
+//   }
+// }
+
+// //каждый раз при добавлении в Payment или в UserWithPayment, мы будем ухудшать код, за счет усложнение и связанность кода.
+// class Payment {
+//   date: Date;
+// }
+
+// class UserWithPayment extends Payment {
+//   name: string;
+// }
+
+// //как правильно, у Payment своя доменная область и у User своя
+// class UserWithPayment2 {
+//   user: User;
+//   payment: Payment;
+//   constructor(user: User, payment: Payment) {
+//     this.user = user;
+//     this.payment = payment;
+//   }
+// }
+
+// // Итог - наследование лучше использовать когда мы наследемся в рамках ОДНОЙ доменной области
+// // наследоваться не надо, когда мы наследуемся от агрегационных сложных внутренних массивов, по типу class Users extends Array<User>.
