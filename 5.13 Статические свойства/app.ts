57bb2c1e1be691acdb047ba4cb91e3198a54ff38
Andrey G <a.gavrilov@promo-bot.ru>
2022-08-14 18:13:47 +0500

    Update app.ts

--- 5.13 Статические свойства/app.ts
+++ 5.13 Статические свойства/app.ts
@@ -1,5 +1,13 @@
-class name {
-	constructor(parameters) {
-		
-	}
+class UserService {
+  static db: any;
+  private static db2: any;
+
+  static getUser(id: number) {
+    return this.db.findById(id);
+  }
 }
+
+//к db можно обратиться без создания класса
+UserService.db;
+
+UserService.getUser(1);
